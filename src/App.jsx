3b146d8debcfd92bc72e70599384c0f816c79c53
Arham5503 <arhamali4510@gmail.com>
2025-09-17 20:33:36 +0500

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -20,31 +20,33 @@ function App() {
 
   return (
     <>
-      <main className="bg-[#1f2632] h-dvh flex items-center justify-center ">
-        <div className="bg-[#4e5d73d2] relative w-96 p-5 flex flex-col items-center">
-          <h3 className="text-[#52ffa8]">Advice# {advice.id} </h3>
-          <h1 className="text-[28px] font-bold text-center text-[#cee3e9]">
-            ❝{advice.quote}❞
-          </h1>
-          <div className="my-2">
-            <img src={divider} alt="" />
-          </div>
-          <button
-            className="bg-[#52ffa8] absolute bottom-0 left-1/2 transform -translate-x-1/2 translate-y-1/2 
+      <main className="bg-[#1f2632]   ">
+        <div className="h-dvh flex items-center justify-center">
+          <div className="bg-[#4e5d73d2] relative w-96 p-5 flex flex-col items-center">
+            <h3 className="text-[#52ffa8]">Advice# {advice.id} </h3>
+            <h1 className="text-[28px] font-bold text-center text-[#cee3e9]">
+              ❝{advice.quote}❞
+            </h1>
+            <div className="my-2">
+              <img src={divider} alt="" />
+            </div>
+            <button
+              className="bg-[#52ffa8] absolute bottom-0 left-1/2 transform -translate-x-1/2 translate-y-1/2 
                        cursor-pointer p-4 rounded-full"
-            onClick={getAdvice}
-          >
-            <img src={dice} alt="" />
-          </button>
+              onClick={getAdvice}
+            >
+              <img src={dice} alt="" />
+            </button>
+          </div>
+        </div>
+        <div className="text-white text-[12px] text-center">
+          <p>
+            Challenge by{" "}
+            <a href="https://www.frontendmentor.io/home">Frontend Mentor</a>.
+            Coded by Arham Ali.
+          </p>
         </div>
       </main>
-      <div>
-        <p>
-          Challenge by{" "}
-          <a href="https://www.frontendmentor.io/home">Frontend Mentor</a>.
-          Coded by Arham Ali.
-        </p>
-      </div>
     </>
   );
 }
